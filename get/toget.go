3a01c9862db83fc8cdbf9e0f62427212a3046225
xmdhs <xmdhss@gmail.com>
2021-04-26 18:12:15 +0800

    🐛

--- get/toget.go
+++ get/toget.go
@@ -3,7 +3,10 @@ package get
 import (
 	"errors"
 	"fmt"
+	"regexp"
 	"strconv"
+	"time"
+	_ "time/tzdata"
 
 	"github.com/yosuke-furukawa/json5/encoding/json5"
 )
@@ -23,11 +26,29 @@ func GetBanData(cid int) (*Baninfo, error) {
 	if err != nil {
 		return &Baninfo{Data: d, Message: t.Message}, fmt.Errorf("GetBanData: %v : %w", string(t.Data), Errjson)
 	}
+	for k := range d {
+		v := d[k]
+		v.Dateline = cover(v.Dateline)
+		d[k] = v
+	}
 	b := Baninfo{
 		Data:    d,
 		Message: t.Message,
 	}
 	return &b, err
+}
+
+var timeReg = regexp.MustCompile(`\d{4}-\d{1,2}-\d{1,2} \d{1,2}:\d{1,2}`)
+
+var cnLoc, _ = time.LoadLocation("")
+
+func cover(t string) string {
+	s := timeReg.FindString(t)
+	ti, err := time.ParseInLocation("2006-1-2 15:04", s, cnLoc)
+	if err != nil {
+		panic(err)
+	}
+	return strconv.FormatInt(ti.Unix(), 10)
 }
 
 var Errjson = errors.New("json err")
